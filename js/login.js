1d273effaae89e1f53bb52df4b60f912f57b1416
Guilherme-De-Marchi <guilhermedemarchi13@gmail.com>
2022-11-13 19:02:13 -0300

    update

--- js/login.js
+++ js/login.js
@@ -34,15 +34,63 @@ function sendRegister() {
 
     fetch(url, headers)
         .then((response) => {
-            return response.json()
+            if (response.status != 200) {
+                throw new Error('something went wrong while registering')
+            }
+            return response
         })
         .then((data) => {
-            console.log(data)
+            localStorage.setItem("epolitica-session", response)
+            window.location.href = "home" // vamos arrumar depois
         })
         .catch(function(error) {
             console.log(error)
         })
 }
 
-// vamos testar da pull ai e roda essa fun
-//deu algum erro to vendo aqui
+function sendLogin() {
+    let email = document.getElementById("login-email").value
+    let password = document.getElementById("login-password").value
+
+    let url = `https://cdbf-2804-7f0-bec1-90e0-6810-298d-a4a-4735.sa.ngrok.io/v1/user/login`
+    let headers = {
+        method: 'POST',
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }),
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    }
+
+    fetch(url, headers)
+        .then((response) => {
+            if (response.status != 200) {
+                throw new Error('something went wrong while logging in')
+            }
+            return response
+        })
+        .then((data) => {
+            localStorage.setItem("epolitica-session", response)
+            window.location.href = "home" // vamos arrumar depois
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
+}
+
+
+// esses que eu coloquei "vamos arrumar depoiis" é pq vai ser diferente o valor quando estiver online
+// ahh simm
+
+// agora so tem que fazer o mesmo pro login normal
+// o login com google eu faço jaja
+// bia
+// agora é so fazer o mesmo
+// mas para os ids login-email  e  login-password
+// foi isso kkk
+// vamo testa
+// testa clicando no botao de register e login
+// mas so muda o email pq vai retornar um erro, por ja ter conta criada, se vc testar o register
